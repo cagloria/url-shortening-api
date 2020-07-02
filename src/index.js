ff8b217fc5942d616962e741140b72963ad0637b
cagloria <33134245+cagloria@users.noreply.github.com>
2020-07-02 13:28:01 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const heading = <h1>Shortly</h1>;
+
+ReactDOM.render(heading, document.getElementById("root"));
