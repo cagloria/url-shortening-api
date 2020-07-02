ddda05ec9ab781ab6ad48d48d1647b54cefa5dfb
cagloria <33134245+cagloria@users.noreply.github.com>
2020-07-02 13:57:55 -0700

    Added functionality to print shortened link
    Prints the functional shortened link to the console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const heading = <h1>Shortly</h1>;
+function Input() {
+    const [urlInput, setUrlInput] = useState("");
 
-ReactDOM.render(heading, document.getElementById("root"));
+    function handleChange(event) {
+        setUrlInput(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        fetch("https://rel.ink/api/links/", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ url: urlInput }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                console.log("https://rel.ink/" + data.hashid); // Shortened link
+            })
+            .catch((error) => console.log(error));
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="url"
+                name="input-field"
+                id="input-field"
+                onChange={handleChange}
+            />
+            <input type="submit" value="Shorten It!" />
+        </form>
+    );
+}
+
+ReactDOM.render(<Input />, document.getElementById("root"));
