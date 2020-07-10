20293b48769b8c3b14b91befd1a78f41da6cb1e3
cagloria <33134245+cagloria@users.noreply.github.com>
2020-07-10 14:01:01 -0700

    Each item now indicates if it was the last copied
    - Each URL item's copy button will change style and text to indicate it was the last one copied
    - Created .button--copied class
    - Renamed a few component variables, props, and states
    - Wrote descriptions for each function
    - Removed variable that renders URL items from Form component and placed in the render function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import $ from "jquery";
 
-function URLItem({ jsonObj }) {
+function URLItem({ jsonObj, onItemCopy, isCopied }) {
+    /**
+     * Copies the shortened URL of this item to the user's clipboard. Lifts the
+     * item's id to Form to show that this item was the last one copied.
+     */
     function copyToClipboard() {
+        // Lift the item's id to Form
+        onItemCopy(jsonObj.id);
+
         // Create temporary input field to copy text from
         let $text = $("<input>");
         $text.val(jsonObj.shortened).css("filter", "opacity(0)");
@@ -18,41 +25,59 @@ function URLItem({ jsonObj }) {
         <li>
             <p className="url-list__original">{jsonObj.original}</p>
             <p className="url-list__shortened">{jsonObj.shortened}</p>
-            <button onClick={copyToClipboard} className="button">
-                Copy
+            <button
+                onClick={copyToClipboard}
+                className={"button" + (isCopied ? " button--copied" : "")}
+            >
+                {isCopied ? "Copied!" : "Copy"}
             </button>
         </li>
     );
 }
 
 function URLInput({ onUrlSubmit }) {
-    const [input, setInput] = useState("");
-    // const [error, setError] = useState("");
-    const [error, setError] = useState("Placeholder error"); // TODO: Delete after testing
+    const [inputValue, setInputValue] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
+    /**
+     * Handles the change of the URL input field. inputValue is changed to the
+     * input field's value, and any change resets errorMessage.
+     * @param {Object} event    URL input field
+     */
     function handleChange(event) {
-        setInput(event.target.value);
-        setError("");
+        setInputValue(event.target.value);
+        setErrorMessage("");
     }
 
+    /**
+     * Requests a shortened URL from the API, using the value of the URL input
+     * field. onUrlSubmit lifts its arguments to Form's shortenUrl() function.
+     * If there is an error, the error message will display it.
+     */
     function getShortenedURL() {
-        $.post("https://rel.ink/api/links/", { url: input })
+        $.post("https://rel.ink/api/links/", { url: inputValue })
             .done(function (data) {
-                onUrlSubmit(input, "https://rel.ink/" + data.hashid);
+                onUrlSubmit(inputValue, "https://rel.ink/" + data.hashid);
             })
             .fail(function (xhr, status, error) {
                 console.log(xhr);
-                setError("Error: " + xhr.status + ". " + xhr.responseText);
+                setErrorMessage(
+                    "Error: " + xhr.status + ". " + xhr.responseText
+                );
             });
     }
 
+    /**
+     * Handles the form submission. Checks if the input field is empty.
+     * @param {Object} event    URL input field
+     */
     function handleSubmit(event) {
         event.preventDefault();
-        if (input === "") {
-            setError("Please add a link");
+        if (inputValue === "") {
+            setErrorMessage("Please add a link");
         } else {
             getShortenedURL();
-            setInput("");
+            setInputValue("");
             event.target.reset();
         }
     }
@@ -65,13 +90,13 @@ function URLInput({ onUrlSubmit }) {
                 id="input-field"
                 className={
                     "input-field" +
-                    (error.length > 0 ? " input-field--invalid" : "")
+                    (errorMessage.length > 0 ? " input-field--invalid" : "")
                 }
                 onChange={handleChange}
                 placeholder="Shorten a link here..."
             />
             <p className="url-form__error-message">
-                {error.length > 0 ? error : null}
+                {errorMessage.length > 0 ? errorMessage : null}
             </p>
             <input type="submit" value="Shorten it!" className="button" />
         </form>
@@ -80,29 +105,23 @@ function URLInput({ onUrlSubmit }) {
 
 function Form() {
     const [newId, updateId] = useState(1);
-    // const [urlList, setUrlList] = useState([]);
-    const [urlList, setUrlList] = useState([
-        {
-            id: 1,
-            original: "https://fonts.google.com/",
-            shortened: "https://fonts.google.com/",
-        },
-        {
-            id: 2,
-            original: "https://fonts.google.com/",
-            shortened: "https://fonts.google.com/",
-        },
-        {
-            id: 3,
-            original: "https://fonts.google.com/",
-            shortened: "https://fonts.google.com/",
-        },
-    ]); // TODO: Delete after testing
+    const [urlList, setUrlList] = useState([]);
+    const [lastCopiedId, setLastCopiedId] = useState(0);
 
-    const listItems = urlList.map((url) => (
-        <URLItem jsonObj={url} key={url.id} />
-    ));
+    /**
+     * Sets lastCopiedId to the ID of the last copied item, lifted from URLItem.
+     * @param {Number} id   ID of the last copied item
+     */
+    function handleCopy(id) {
+        setLastCopiedId(id);
+    }
 
+    /**
+     * Creates a JSON object of an id, the original URL, and the shortened URL,
+     * and pushes it into urlList.
+     * @param {String} inputUrl     User input URL
+     * @param {String} shortenedURL Shortened URL
+     */
     function shortenUrl(inputUrl, shortenedURL) {
         setUrlList(
             urlList.concat({
@@ -113,11 +132,21 @@ function Form() {
         );
         updateId(newId + 1);
     }
+
     return (
         <section className="form-section section-padding">
             <div className="form-section-content">
                 <URLInput onUrlSubmit={shortenUrl} />
-                <ul className="url-list">{listItems}</ul>
+                <ul className="url-list">
+                    {urlList.map((urlObj) => (
+                        <URLItem
+                            jsonObj={urlObj}
+                            key={urlObj.id}
+                            onItemCopy={handleCopy}
+                            isCopied={lastCopiedId === urlObj.id}
+                        />
+                    ))}
+                </ul>
             </div>
         </section>
     );
